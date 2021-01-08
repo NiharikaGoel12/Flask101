779df530046dab3ef3318b074574f5afcad11706
Niharika Goel <niharika.coer@gmail.com>
2021-01-08 10:28:26 +0530

    Sports registration form

--- hello/app.py
+++ hello/app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+SPORTS = ["TT", "CRICKET", "BASEBALL", "FOOSEBALL", "BADMINTON"]
 
+@app.route("/")
 def index():
-    if request.method =="POST":
-        return render_template("greet.html", name= request.form.get("name", "world"))
-    return render_template("index.html")
+    return render_template("index.html", sports_list= SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        # Decline user registration
+        return render_template("failure.html")
+
+    # Confirm user registration
+    return render_template("success.html")
+
